b596a0cc6d89400897d9b1599f2b749e6267cfc5
Andrew Rothstein <andrew.rothstein@gmail.com>
2015-02-16 21:50:38 -0500

    require a ENV and CLUSTER environment to determine the DNS domain to find peers in

--- boot.sh
+++ boot.sh
@@ -2,4 +2,7 @@
 
 printf "${ZK_MYID}" > /zk-data/myid
 printf "starting ZK instance $(cat /zk-data/myid)..."
+
+printf "installing in domain ${CLUSTER}.${ENV}.local..."
+sed -i "s/ZK_DNS_DOMAIN/${CLUSTER}.${ENV}.local/" /opt/zookeeper/conf/zoo.cfg
 /opt/zookeeper/bin/zkServer.sh start-foreground

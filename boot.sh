b026c2b5f139953d1fcaebc5326efa456f7e28f3
Andrew Rothstein <andrew.rothstein@gmail.com>
2015-02-16 23:18:26 -0500

    more helpful boot up debuggery

--- boot.sh
+++ boot.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
 printf "${ZK_MYID}" > /zk-data/myid
-printf "starting ZK instance $(cat /zk-data/myid)..."
+echo starting ZK instance $(cat /zk-data/myid)...
 
-printf "installing in domain ${CLUSTER}.${ENV}.local..."
+echo installing in domain ${CLUSTER}.${ENV}.local...
 sed -i "s/ZK_DNS_DOMAIN/${CLUSTER}.${ENV}.local/" /opt/zookeeper/conf/zoo.cfg
+
+echo booting configuration:
+cat /opt/zookeeper/conf/zoo.cfg
+
+echo launching...
 /opt/zookeeper/bin/zkServer.sh start-foreground

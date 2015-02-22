1f01d4fbc32f25bcb883b2d8fc7dd784a1bd2bcd
Andrew Rothstein <andrew.rothstein@gmail.com>
2015-02-21 21:12:22 -0500

    more debuggery

--- boot.sh
+++ boot.sh
@@ -2,15 +2,24 @@
 
 printf "${ZK_MYID}" > /zk-data/myid
 echo starting ZK instance $(cat /zk-data/myid)...
-
 echo installing in domain ${CLUSTER}.${ENV}.local...
 sed -i "s/ZK_DNS_DOMAIN/${CLUSTER}.${ENV}.local/" /opt/zookeeper/conf/zoo.cfg
-
+echo
+echo digging for peers...
+dig zookeeper-1.${CLUSTER}.${ENV}.local
+dig zookeeper-2.${CLUSTER}.${ENV}.local
+dig zookeeper-3.${CLUSTER}.${ENV}.local
+echo
+echo pinging my peers...
+ping -c 5 zookeeper-1.${CLUSTER}.${ENV}.local
+ping -c 5 zookeeper-2.${CLUSTER}.${ENV}.local
+ping -c 5 zookeeper-3.${CLUSTER}.${ENV}.local
+echo
 echo booting configuration:
 cat /opt/zookeeper/conf/zoo.cfg
-
-echo netstats:
+echo
+echo pre-boot netstats:
 netstat -l
-
-echo launching...
+echo
+echo launching zookeeper...
 /opt/zookeeper/bin/zkServer.sh start-foreground
